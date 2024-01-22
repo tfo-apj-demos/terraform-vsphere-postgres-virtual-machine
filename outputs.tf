dd6e22533822f7a3615532c693bb6affeaa2fe91
Aaron Evans <aarone@outlook.com.au>
2024-01-22 16:37:18 +1100

    update module

--- outputs.tf
+++ outputs.tf
@@ -10,7 +10,7 @@ output "vsphere_compute_cluster_id" {
 
 output "virtual_machine" {
   value = tomap({
-    for k, v in module.vm : k => v {
+    for k, v in module.vm : k => {
       id = v.virtual_machine_id
       virtual_machine_name = v.virtual_machine_name
       ip_address = v.ip_address

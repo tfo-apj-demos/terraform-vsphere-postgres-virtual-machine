6c36bc8098c796ff6baec69eaade2d2c7fcaf632
Aaron Evans <aarone@outlook.com.au>
2024-01-24 09:53:44 +1100

    update outputs

--- outputs.tf
+++ outputs.tf
@@ -20,7 +20,7 @@ output "ip_address" {
 
 output "virtual_machine_details" {
   value = {
-    for idx, vm in local.vms : idx => {
+    for idx, vm in local.vms : vm.virtual_machine_name => {
       virtual_machine_name = vm.virtual_machine_name
       ip_address           = vm.ip_address
     }

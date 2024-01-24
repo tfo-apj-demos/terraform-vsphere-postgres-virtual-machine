49e4adcecbc85d165af1e8416290faa3e28c8250
Aaron Evans <aarone@outlook.com.au>
2024-01-25 10:14:02 +1100

    updates

--- example/main.tf
+++ example/main.tf
@@ -8,6 +8,44 @@ terraform {
     }
   }
 }
+
+module "postgres-virtual-machine" { 
+  source = "app.terraform.io/tfo-apj-demos/postgres-virtual-machine/vsphere"
+  version = "~> 1"
+  
+  backup_policy    = "daily"
+  environment      = "prod"
+  site             = "canberra"
+  size             = "large"
+  storage_profile  = "performance"
+  tier             = "gold"
+  database_profile = "public-access"
+}
+
+output "name" {
+  value = module.postgres-virtual-machine.virtual_machine_name
+}
+
+output "virtual_machine_details" {
+  value = module.postgres-virtual-machine.virtual_machine_details
+}
+
+/*module "domain-name-system-management" {
+  source  = "app.terraform.io/tfo-apj-demos/domain-name-system-management/dns"
+  version = "~> 1.0"
+
+  a_records = [{
+    name      = module.postgres-virtual-machine.virtual_machine_name
+    addresses = [module.postgres-virtual-machine.ip_address]
+  }]
+  a_records = [ for vm in module.postgres-virtual-machine.virtual_machine: {
+    "name" = vm.virtual_machine_name
+    "addresses" = [vm.ip_address]
+  }]
+}*/
+
+
+/*
 variable "boundary_address" {
   type = string
 }
@@ -20,37 +58,13 @@ provider "boundary" {
   token = var.boundary_token
 }
 
-module "postgres-virtual-machine" {
-  source  = "app.terraform.io/tfo-apj-demos/postgres-virtual-machine/vsphere"
-  version = "~> 1"
-
-  backup_policy    = "daily"
-  environment      = "prod"
-  site             = "canberra"
-  size             = "large"
-  storage_profile  = "performance"
-  tier             = "gold"
-  database_profile = "public-access"
-}
-
-module "domain-name-system-management" {
-  source  = "app.terraform.io/tfo-apj-demos/domain-name-system-management/dns"
-  version = "~> 1.0"
-
-  a_records = [{
-    name      = module.postgres-virtual-machine.virtual_machine_name
-    addresses = [module.postgres-virtual-machine.ip_address]
-  }]
-}
-
-
 module "boundary_target" {
   source  = "app.terraform.io/tfo-apj-demos/target/boundary"
   version = "~> 0.0"
 
-  hosts = [{
-    "hostname" = module.postgres-virtual-machine.virtual_machine_name
-    "address" = module.postgres-virtual-machine.ip_address
+   hosts = [ for vm in module.postgres-virtual-machine.virtual_machine_details : {
+    "hostname" = vm.virtual_machine_name
+    "address" = vm.ip_address
   }]
 
   services = [
@@ -62,10 +76,6 @@ module "boundary_target" {
   ]
   project_name = "grantorchard"
   host_catalog_id = "hcst_7B2FWBRqb0"
-  hostname_prefix = "remote-desktop"
+  hostname_prefix = "postgres-access"
   injected_credential_library_ids = []
-}
-
-output "vm_name" {
-  value = module.postgres-virtual-machine.virtual_machine_name
-}
+}*/

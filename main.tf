88e9ccbd41020503cfc7fac2a651b1993d079729
Aaron Evans <aarone@outlook.com.au>
2025-05-15 13:17:30 +1000

    Update module version in main.tf to 1.4.1

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "vm" {
   source  = "app.terraform.io/tfo-apj-demos/virtual-machine/vsphere"
-  version = "~> 1.4"
+  version = "1.4.1"
 
   template          = data.hcp_packer_artifact.this.external_identifier
   hostname          = var.hostname
